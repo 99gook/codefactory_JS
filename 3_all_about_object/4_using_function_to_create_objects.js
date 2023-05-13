e67014a212fb231d7a42242c4c1dd630c3a5c186
99gook <121601501+99gook@users.noreply.github.com>
2023-05-13 17:49:57 +0900

    Update 4_using_function_to_create_objects.js

--- 3_all_about_object/4_using_function_to_create_objects.js
+++ 3_all_about_object/4_using_function_to_create_objects.js
@@ -2,8 +2,8 @@
  * Using function to create objects
  */
 function IdolModel(name, year) {
-    if (!new.target) {
-        return new IdolModel(name, year);
+    if (!new.target) { // new.target 사용 시 new 선언된 함수는 함수를 return 하고, new로 선언 x 는 undefined가 return
+        return new IdolModel(name, year); // 이렇게 if 안에서 new를 return 할 수도 있음 -> 함수 안에서 같은 함수 실행
     }
 
     this.name = name;
@@ -20,6 +20,16 @@ console.log(yuJin);
 const yuJin2 = IdolModel('안유진', 2003);
 console.log(yuJin2);
 // console.log(global.name);
+
+
+/**
+ * 위 처럼 함수를 정의하면 new를 사용하던 사용하지 않던 원하는 함수를 return 할 수 있는 장점이 있음
+ */
+
+
+/**
+ * arrow 함수에서는 생성자 함수 사용 불가능함
+ */
 
 const IdolModelArrow = (name, year) => {
     this.name = name;
